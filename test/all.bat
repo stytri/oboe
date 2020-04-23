b4c8efebd793124e716a84ff2e5a6f59efe74d76
stytri <oboe-git-scm@tstyles.eclipse.co.uk>
2020-04-23 13:26:42 +0100

    Added separate basic testing

--- test/all.bat
+++ test/all.bat
@@ -1,21 +1,4 @@
-@call %1 hello-world        %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-logic        %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-bitwise      %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-arithmetic   %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-comparison   %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-alternation  %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-assignment   %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-iteration    %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-function     %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-array        %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-unicode      %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-string       %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-character    %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-arguments    %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-quote+eval   %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-op-function  %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-op-alias     %2 %3 %4 %5 %6 %7 %8 %9
-@call %1 basic-file         %2 %3 %4 %5 %6 %7 %8 %9
-
+@call %1 hello-world            %2 %3 %4 %5 %6 %7 %8 %9
+@call basic                  %1 %2 %3 %4 %5 %6 %7 %8 %9
 @call %1 rosetta\100-doors      %2 %3 %4 %5 %6 %7 %8 %9
 @call %1 rosetta\100-prisoners  %2 %3 %4 %5 %6 %7 %8 %9
---
+++ test/basic.bat
@@ -0,0 +1,1 @@
+@for %%f IN (basic-*.oboe) DO @ call %1 %%~nf %2 %3 %4 %5 %6 %7 %8 %9
