24386f0de5403ca7a634e26a86105676efa6d0d1
stytri <oboe-git-scm@tstyles.eclipse.co.uk>
2020-04-24 21:46:57 +0100

    Added rosetta/jensens-device

--- test/all.bat
+++ test/all.bat
@@ -2,3 +2,4 @@
 @call basic                  %1 %2 %3 %4 %5 %6 %7 %8 %9
 @call %1 rosetta\100-doors      %2 %3 %4 %5 %6 %7 %8 %9
 @call %1 rosetta\100-prisoners  %2 %3 %4 %5 %6 %7 %8 %9
+@call %1 rosetta\jensens-device %2 %3 %4 %5 %6 %7 %8 %9
