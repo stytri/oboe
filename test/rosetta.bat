540e6ff6116aaf574f3a9fd276dbf726e56f617d
stytri <oboe-git-scm@tstyles.eclipse.co.uk>
2020-05-08 15:02:46 +0100

    Added rosetta tree-traversal

--- test/rosetta.bat
+++ test/rosetta.bat
@@ -1,6 +1,7 @@
 @call %1 rosetta\100-doors
 @call %1 rosetta\lah-numbers
 @call %1 rosetta\priority-queue
+@call %1 rosetta\tree-traversal
 @call %1 rosetta\knapsack-problem-0-1
 @call %1 rosetta\determine-string-unique-characters "abcdefg" "abcdcfg" "abcdcbc" "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ" "1234567890ABCDEFGHIJKLMN0PQRSTUVWXYZ" "1234567890ABCD3FGH1JKLMN0PQRSTUVWXYZ"
 @call %1 rosetta\aliquot-sequence-classifications -rosetta\aliquot.in
