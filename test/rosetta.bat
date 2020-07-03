f4622cdfd015dbf4928367bef6d71d803abcb282
stytri <63111134+stytri@users.noreply.github.com>
2020-07-03 12:59:42 +0100

    Added prime-decomposition to rosetta tests

--- test/rosetta.bat
+++ test/rosetta.bat
@@ -6,5 +6,6 @@
 @call %1 rosetta\determine-string-unique-characters "abcdefg" "abcdcfg" "abcdcbc" "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ" "1234567890ABCDEFGHIJKLMN0PQRSTUVWXYZ" "1234567890ABCD3FGH1JKLMN0PQRSTUVWXYZ"
 @call %1 rosetta\aliquot-sequence-classifications -rosetta\aliquot.in
 @call %1 rosetta\Gaussian-elimination
+@call %1 rosetta\prime-decomposition
 @call %1 rosetta\jensens-device
 @call %1 rosetta\100-prisoners
